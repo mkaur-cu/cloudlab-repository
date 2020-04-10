19867846371e2703b073a0ec0a11d4e9c8c22b90
mkaur-cu <63457941+mkaur-cu@users.noreply.github.com>
2020-04-10 11:03:29 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -9,12 +9,6 @@ node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
 node.routable_control_ip = "true"
 
 node.addService(rspec.Execute(shell="/bin/sh",
-                              command="sudo apt update"))
-node.addService(rspec.Execute(shell="/bin/sh",
-                              command="sudo apt install -y apache2"))
-node.addService(rspec.Execute(shell="/bin/sh",
-                              command='sudo uwf allow in "Apache Full"'))
-node.addService(rspec.Execute(shell="/bin/sh",
-                              command='sudo systemctl status apache2'))
+                              command="sudo bash install_webserver.sh"))
 # Print the RSpec to the enclosing page.
 portal.context.printRequestRSpec()
